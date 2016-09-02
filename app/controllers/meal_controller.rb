f910e64c6880f73b4619d80d3708552b62cfcdca
ebanh <ebanhernandez@gmail.com>
2016-09-02 13:28:21 +0900

    modified new route to handle capitalization inconsistancies

--- app/controllers/meal_controller.rb
+++ app/controllers/meal_controller.rb
@@ -28,10 +28,10 @@ class MealController < ApplicationController
     end
 
     day = user.days.find_by(date: params[:day][:date], meal_time: params[:day][:meal_time]) || user.days.create(params[:day])
+    meal = user.meals.where('lower(name)=?', params[:meal][:name].downcase).first
+    meal ||= user.meals.create(params[:meal])
 
-    meal = user.meals.find_by(name: params[:meal][:name]) || user.meals.create(params[:meal])
     day.meals << meal
-
     redirect to "/meals/list/#{user.slug}"
   end
 
